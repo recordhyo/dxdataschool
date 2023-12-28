f2dfb19bdab9a1e0e8964485c2e5ad78351e8adf
recordhyo <gytla1@naver.com>
2023-12-28 14:55:34 +0900

    231228 파일 정리

---
+++ 1010springdockerfile/src/main/java/com/dockerfile/springdockerfile/SpringdockerfileApplication.java
@@ -0,0 +1,13 @@
+package com.dockerfile.springdockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockerfileApplication.class, args);
+	}
+
+}
